2b1e6abcbbe0cdd96c95590f064139b557cabee6
Aaron <jerseypaint@gmail.com>
2020-06-23 13:46:43 -0400

    Spotify plugin now pulls all episodes

--- plugins/gatsby-source-spotify/gatsby-node.js
+++ plugins/gatsby-source-spotify/gatsby-node.js
@@ -11,10 +11,9 @@
  *
  * See: https://www.gatsbyjs.org/docs/creating-a-local-plugin/#developing-a-local-plugin-that-is-outside-your-project
  */
-exports.onPreInit = (_, pluginOptions) => console.log(pluginOptions.clientId)
+exports.onPreInit = (_, pluginOptions) => console.log("Loaded Spotify source plugin")
 
 const axios = require('axios');
-
 const EPISODE_NODE_TYPE = `Episode`
 
 exports.sourceNodes = async ({
@@ -22,19 +21,48 @@ exports.sourceNodes = async ({
     createContentDigest,
     createNodeId,
     getNodesByType,
-  },
-  pluginOptions
-  ) => {
+  },   pluginOptions ) => {
     const { createNode, touchNode, deleteNode } = actions
-    
+
+    let url = `https://api.spotify.com/v1/shows/2vClZVAJHI7mZZjVycUXzP/episodes`
+    const episodes = []
+
     const clientId = pluginOptions.clientId
     const clientSecret = pluginOptions.clientSecret
     const auth64 = Buffer.from(clientId + `:`+ clientSecret).toString('base64')
+
+    const buildArray = (response) => {
+      episodes.push(...response.data.items)
     
-    const client = () => {
-      
+      if (response.data.next !== null) {
+        return client(response.data.next)
+      }
     
-        return axios({
+      return episodes
+    }
+    
+    const request = (response, url) => {
+      return axios({
+        url: url,
+        method: 'get',
+        params: {
+            market: 'US',
+            limit: 50
+        },
+        headers: {
+            'Accept':'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + response.data.access_token
+        }
+      }).then((response) => {
+        return buildArray(response)
+      }).catch(function(error){
+        console.log(error)
+      })
+    }
+    
+    const client = (url) => {
+            return axios({
                 url: 'https://accounts.spotify.com/api/token',
                 method: 'post',
                 params: {
@@ -45,36 +73,25 @@ exports.sourceNodes = async ({
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Authorization': 'Basic ' + auth64
                 }
-            }).then(function(response) {
-                return (
-                    axios({
-                        url: 'https://api.spotify.com/v1/shows/2vClZVAJHI7mZZjVycUXzP/episodes',
-                        method: 'get',
-                        params: {
-                            market: 'US',
-                            limit: 50
-                        },
-                        headers: {
-                            'Accept':'application/json',
-                            'Content-Type': 'application/json',
-                            'Authorization': 'Bearer ' + response.data.access_token
-                        }
-                    }).then(function(response){
-                        response.data
-                        return response.data
-                    }).catch(function(error){
-                        console.log(error)
-                    })
-                )
+            }).then((response) => {
+    
+              return request(response, url)
+            
             }).catch(function(error) {
+              console.log(error)
             })
-        
     }
     
-    const data = await client()
+    const data = await client(url)
+
+    if (data) {
+      console.log(data.length)
+    } else {
+      console.log(`No Data?`)
+    }
 
     // loop through data and create Gatsby nodes
-    data.items.forEach(item =>
+    data.forEach(item =>
       createNode({
         ...item,
         id: createNodeId(`${EPISODE_NODE_TYPE}-${item.id}`),
