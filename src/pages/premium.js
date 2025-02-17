68ba5a72f1ce0c385bdc60ea832b028e1557cfe2
Aaron D <jerseypaint@gmail.com>
2025-02-17 10:23:23 -0500

    add new plan

--- src/pages/premium.js
+++ src/pages/premium.js
@@ -10,6 +10,17 @@ const Wrapper = styled.div`
   margin: 0 auto;
   text-align: center;
 `
+const Grid = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+  @media (min-width: 640px) {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-gap: 2rem;
+      margin-top: 4rem;
+  }
+`
 
 const IndexPage = () => {
 
@@ -17,13 +28,19 @@ const IndexPage = () => {
   <Layout>
     <SEO title="Support" />
     <Section>
-        <Wrapper>
-            <h2>Support Us</h2>
-            <Script src="https://app.supercast.com/js/embed.js" />
-            <supercast-plan code="993dbcec-93cf-45ce-b981-925fb1828623"></supercast-plan>
-        </Wrapper>
+      <Wrapper>
+        <h2>Support Us</h2>
+        <Script src="https://healthunchained.supercast.com/js/embed.js" />
+      </Wrapper>
+      <div className="container">
+      <Grid>
+        <supercast-plan code="993dbcec-93cf-45ce-b981-925fb1828623"></supercast-plan>
+        <supercast-plan code="e048930f-c556-4365-98e0-55c6857646ce"></supercast-plan>
+      </Grid>
+      </div>
     </Section>
   </Layout>
-)}
+  )
+}
 
 export default IndexPage
